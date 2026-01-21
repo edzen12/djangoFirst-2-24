153ffd5eea385bd676c541d8be06ea461849305f
edzen12 <oichiev.edzen@gmail.com>
2026-01-21 11:28:16 +0600

    add image and add menu

--- djangoFirst/urls.py
+++ djangoFirst/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from news.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage)
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/models.py
+++ news/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class News(models.Model):
     title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='newsMedia/', null=True)
     description = models.TextField()
 
     def __str__(self):
